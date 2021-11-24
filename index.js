b469e5448d0748fd02249ac50b8645c47445e2b1
JonCabot <jfc0053@gmail.com>
2021-11-23 20:01:49 -0600

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
-function saturdayFun(activity="roller-skate") {
+//1
+function saturdayFun(activity = "roller-skate"){
   return `This Saturday, I want to ${activity}!`
 }
+saturdayFun()
 
-let mondayWork = function(activity="go to the office") {
+//2
+const mondayWork = function (activity = "go to the office") {
   return `This Monday, I will ${activity}.`
 }
+mondayWork();
 
-let wrapAdjective = function(style="*") {
-  return function(adjective="special") {
-    return `You are ${style}${adjective}${style}!`
+//3
+let wrapAdjective = function(highLight = "*"){
+  return function(param = "special") {
+    return `You are ${highLight}${param}${highLight}!`
   }
 }
+
+
+/*
+describe("defines wrapAdjective function according to the specification", function() {
+  it("function exists", function() {
+    expect(wrapAdjective).to.exist
+  })
+  
+  it("when initialized with '*' creates a function that, when called, wraps an adjective in a highlight", function() {
+    let result = wrapAdjective('*')
+    let emphatic = result("a hard worker")
+    expect(emphatic).to.equal("You are *a hard worker*!")
+  });
+
+  it("when initialized with '||' creates a function that, when called, wraps an adjective in a highlight", function() {
+    let result = wrapAdjective("||")
+    let emphatic = result("a dedicated programmer")
+    expect(emphatic).to.equal("You are ||a dedicated programmer||!")
+  });
+})
+})
+*/
